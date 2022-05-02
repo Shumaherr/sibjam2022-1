bbe21e897a88500e14f84f03fe4f60ee87c3580c
aleksandr <shumaherr@gmail.com>
2022-05-02 14:34:36 +0200

    changes

--- Sib-jam-2022/Assets/Scripts/Item.cs
+++ Sib-jam-2022/Assets/Scripts/Item.cs
@@ -1,7 +1,21 @@
 ﻿using UnityEngine;
 
+public enum State
+{
+    OnStore,
+    StartMoving,
+    IsMoving,
+    EndMoving,
+    InCar
+}
 public class Item: MonoBehaviour
 {
     protected float x, y;
     protected int width, height;
+    protected State state;
+    
+    protected Item()
+    {
+        state = State.OnStore;
+    }
 }
