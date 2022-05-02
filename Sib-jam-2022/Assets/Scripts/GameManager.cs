77194c5a79a8011421f7358eebdbc7e583d7371b
aleksandr <shumaherr@gmail.com>
2022-05-02 14:33:49 +0200

    grid

--- Sib-jam-2022/Assets/Scripts/GameManager.cs
+++ Sib-jam-2022/Assets/Scripts/GameManager.cs
@@ -6,8 +6,11 @@ using UnityEngine.Tilemaps;
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private Vector2Int inventorySize;
-    Grid grid;
-    
+    [SerializeField] private Transform cellPrefab;
+    private Grid grid;
+    private GameObject gridGO;
+
+
     [SerializeField] private Camera mainCamera;
 
     public Camera MainCamera => mainCamera;
@@ -15,14 +18,25 @@ public class GameManager : Singleton<GameManager>
     // Start is called before the first frame update
     void Start()
     {
-       
+        grid = new Grid(inventorySize.x, inventorySize.y, 1, Vector3.zero);
+        gridGO = new GameObject("Grid");
+        gridGO.transform.position = Vector3.zero;
+        for (int i = 0; i < grid.Width; i++)
+        {
+            for (int j = 0; j < grid.Height; j++)
+            {
+                var cell = Instantiate(cellPrefab, new Vector3(i, j, 0), Quaternion.identity, gridGO.transform);
+                cell.parent = gridGO.transform;
+            }
+        }
+    }
+
+    void DrawCell(Vector3 pos, int size)
+    {
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-    
-    
 }
